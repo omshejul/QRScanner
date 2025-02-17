c85c2684cc71cf51ea63530a9014febcd75da99a
Om’s Mac-Mini <contact@omshejul.com>
2025-02-17 16:21:30 +0530

    Initial Commit

---
+++ QRScanner/QRScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScannerApp.swift
+//  QRScanner
+//
+//  Created by Om Shejul on 17/02/25.
+//
+
+import SwiftUI
+
+@main
+struct QRScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
