4bf57691cf8ae7b33e0d6c94e1f76926ab937c94
Om’s Mac-Mini <contact@omshejul.com>
2025-02-28 10:53:29 +0530

    add default widget

---
+++ ScanWidget/ScanWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  ScanWidgetBundle.swift
+//  ScanWidget
+//
+//  Created by Om Shejul on 28/02/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ScanWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ScanWidget()
+        ScanWidgetControl()
+    }
+}
